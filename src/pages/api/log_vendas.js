ad2537a88cf585d1e86e0e3cae525ad57207a7ea
Victttor <victor.emanuell.s.p@hotmail.com>
2024-04-22 20:43:13 -0400

    commit

--- src/pages/api/log_vendas.js
+++ src/pages/api/log_vendas.js
@@ -1,8 +1,5 @@
-import { createUser, getUserByEmail } from "@/repositorio/user.repo.mjs";
 import {authOptions} from "./auth/[...nextauth]"
 import { getServerSession } from "next-auth/next"
-import { getDepartments } from "@/repositorio/categ_ingresso.repo.mjs";
-import { getProducts } from "@/repositorio/product.repo.mjs";
 
 export default async function handler(req, res) {
     const session = await getServerSession(req, res, authOptions)
@@ -10,15 +7,15 @@ export default async function handler(req, res) {
         console.log("session.user", session.user);
     }
     if (req.method === 'GET') {
-        return handleGetProducts(req, res);
+        return handleGetLog(req, res);
     }
     res.status(405).send({});
 }
 
-async function handleGetProducts(req, res) {
+async function handleGetLog(req, res) {
     try {
-        let prods = await getProducts();
-        res.status(200).send(prods);
+        let logs = await getLog();
+        res.status(200).send(logs);
     } catch(err) {
         console.error(err);
         res.status(500).send({message: err.message});
