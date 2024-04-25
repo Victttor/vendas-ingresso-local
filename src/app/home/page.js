f7088a043bb2daba98c74440b2c2256427933c59
Victttor <victor.emanuell.s.p@hotmail.com>
2024-04-25 18:49:46 -0400

    commit

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -95,73 +95,11 @@ function classNames(...classes) {
 
 export default function Home() {
 
-    /*const [products, setProducts] = useState(_products);
-    const router = useRouter();
-
-    useEffect(() => {
-        api.get('/products')
-            .then(response => console.log(response.data));
-        // api.get('/products')
-        //     .then(response => setProducts(response.data));
-        
-        // api.get('/customers')
-        //     .then(response => {
-        //         let customer = response.data;
-        //         if (customer?.address1==null || customer?.address1=='') {
-        //             router.push('/home/profile');
-        //         }
-        //     });
-    }, []);*/
-
     return (
         <section aria-labelledby="trending-heading" className="bg-white">
             <div className="py-4 sm:py-24 lg:mx-auto lg:max-w-7xl lg:px-8 lg:py-8">
                 <div className="border-t -mx-px grid grid-cols-2 border-l border-gray-200 sm:mx-0 md:grid-cols-3 lg:grid-cols-4">
-                    {/*products.map((product) => (
-                    <div
-                        key={product.id}
-                        className="group relative border-b border-r border-gray-200 p-4 sm:p-6"
-                    >
-                        <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-gray-200 group-hover:opacity-75">
-                        <img
-                            src={product.imageSrc}
-                            alt={product.imageAlt}
-                            className="h-full w-full object-cover object-center"
-                        />
-                        </div>
-                        <div className="pb-4 pt-10 text-center">
-                        <h3 className="text-sm font-medium text-gray-900">
-                            <a href={product.href}>
-                            <span aria-hidden="true" className="absolute inset-0" />
-                            {product.name}
-                            </a>
-                        </h3>
-                        <div className="mt-3 flex flex-col items-center">
-                            <p className="sr-only">{product.rating} out of 5 stars</p>
-                            <div className="flex items-center">
-                            {[0, 1, 2, 3, 4].map((rating) => (
-                                <StarIcon
-                                key={rating}
-                                className={classNames(
-                                    product.rating > rating
-                                    ? "text-yellow-400"
-                                    : "text-gray-200",
-                                    "h-5 w-5 flex-shrink-0"
-                                )}
-                                aria-hidden="true"
-                                />
-                            ))}
-                            </div>
-                            <p className="mt-1 text-sm text-gray-500">
-                            {product.reviewCount} reviews
-                            </p>
-                        </div>
-                        <p className="mt-4 text-base font-medium text-gray-900">
-                            {product.price}
-                        </p>
-                        </div>
-                    </div>
-                    ))*/}
+                    {}
                 </div>
             </div>
         </section>
